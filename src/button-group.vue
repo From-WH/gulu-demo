61dd4823644a06afa540a97645b3f1e50daf935d
wanghang <from-wh@hotmail.com>
2018-08-31 21:55:57 +0800

    完成button-group创建

---
+++ src/button-group.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script></script>
+<style lang="scss">
+  .g-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+    > .g-button {
+      border-radius: 0;
+      margin-left: -1px;
+      &:first-child {
+        border-bottom-left-radius: 4px;
+        border-top-left-radius: 4px;
+      }
+      &:last-child {
+        border-bottom-right-radius: 4px;
+        border-top-right-radius: 4px;
+      }
+      &:hover {
+        z-index: 1;
+      }
+    }
+  }
+</style>
