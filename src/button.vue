fcbcbe8f47eeb9aac290377ded01ba8222bd23ea
wanghang <from-wh@hotmail.com>
2018-09-02 11:31:34 +0800

    使用karma做自动化测试

--- src/button.vue
+++ src/button.vue
@@ -9,7 +9,11 @@
 </template>
 
 <script>
+  import Icon from './icon'
   export default {
+    components:{
+      'g-icon':Icon,
+    },
     // props:['icon','iconPosition']
     props: {
       icon: {},
