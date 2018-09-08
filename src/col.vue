b69e3e264b4704ec284a8786817c566468476aab
wanghang <from-wh@hotmail.com>
2018-09-08 15:57:30 +0800

    col 和 row 初步完成

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'GuluCol',
+    props: {
+      span: {type: [Number, String]}
+    }
+  }
+
+</script>
+<style scoped lang="scss">
+  .col {
+    height: 100px;
+    width: 50%;
+    background: #333;
+    border: 1px solid red;
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24)*100%;
+      }
+    }
+  }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+<style scoped lang="scss">
+  .row{
+    display: flex;
+    margin: 20px 0;
+  }
+</style>
