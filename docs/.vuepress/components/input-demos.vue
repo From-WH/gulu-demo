9d0f0764e7391c8dba9f09ba8f59c9500a18d23b
wanghang <from-wh@hotmail.com>
2018-09-30 16:36:06 +0800

    官网input 组件展示完成

---
+++ docs/.vuepress/components/input-demos.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div class="box" style="margin-bottom: 20px;">
+      <g-input value="张三"></g-input>
+      <g-input value="张三" disabled></g-input>
+      <g-input value="张三" readonly></g-input>
+    </div>
+    <div class="box">
+      <g-input value="张二" error="姓名不能小于两个字"></g-input>
+    </div>
+    <br>
+
+    <div class="box">
+      <h3>双向绑定</h3>
+      <g-input v-model="value"></g-input>
+      <div>
+        value: {{value}}
+      </div>
+    </div>
+    <h3>代码:</h3>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<script>
+  import Input from '../../../src/input'
+  import Icon from '../../../src/icon'
+
+
+
+  export default {
+    components: {
+      'g-icon': Icon,
+      'g-input': Input
+    },
+
+    data() {
+      return {
+        value: '1',
+        content: `
+    <div class="box" style="margin-bottom: 20px;">
+      <g-input value="张三"></g-input>
+      <g-input value="张三" disabled></g-input>
+      <g-input value="张三" readonly></g-input>
+    </div>
+    <div class="box">
+      <g-input value="张二" error="姓名不能小于两个字"></g-input>
+    </div>
+    <div class="box">
+      <g-input value="张四" @change="inputChange"></g-input>
+    </div>
+`.trim()
+      }
+    }
+  }
+
+</script>
--- docs/components/input.md
+++ docs/components/input.md
@@ -3,3 +3,7 @@ title: Input - 输入框
 ---
 
 # 输入框
+
+<ClientOnly>
+<input-demos></input-demos>
+</ClientOnly>
