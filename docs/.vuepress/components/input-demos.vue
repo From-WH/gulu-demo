2fc3a46d0238310b349e159bede6214ba69e07c3
wanghang <from-wh@hotmail.com>
2018-09-30 16:38:39 +0800

    更新input代码展示内容

--- docs/.vuepress/components/input-demos.vue
+++ docs/.vuepress/components/input-demos.vue
@@ -35,19 +35,28 @@
 
     data() {
       return {
-        value: '1',
+        value: '车轮滚滚',
         content: `
-    <div class="box" style="margin-bottom: 20px;">
-      <g-input value="张三"></g-input>
-      <g-input value="张三" disabled></g-input>
-      <g-input value="张三" readonly></g-input>
-    </div>
-    <div class="box">
-      <g-input value="张二" error="姓名不能小于两个字"></g-input>
-    </div>
-    <div class="box">
-      <g-input value="张四" @change="inputChange"></g-input>
-    </div>
+<div>
+  <g-input value="张三"></g-input>
+  <g-input value="张三" disabled></g-input>
+  <g-input value="张三" readonly></g-input>
+</div>
+<div>
+  <g-input value="张二" error="姓名不能小于两个字"></g-input>
+</div>
+
+<div>
+  <h3>双向绑定</h3>
+  <g-input v-model="value"></g-input>
+  <div>
+    value: {{value}}
+  </div>
+</div>
+
+data:{
+value: '1',
+}
 `.trim()
       }
     }
