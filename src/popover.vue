494d2642fe5bf57029c920d9d2908912495dffea
wanghang <from-wh@hotmail.com>
2018-10-13 13:20:38 +0800

    popover不会受overflow影响

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
+    <span ref="triggerWrapper">
     <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -13,13 +15,20 @@
     data() {
       return {visible: false}
     },
+    mounted() {
+
+    },
     methods: {
       xxx() {
         this.visible = !this.visible
 
         if (this.visible === true) {
           this.$nextTick(() => {
-            let eventHandler = ()=>{
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + 'px'
+            this.$refs.contentWrapper.style.top = top + 'px'
+            let eventHandler = () => {
               this.visible = false
               document.removeEventListener('click', eventHandler)
             }
@@ -36,11 +45,11 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-      position: absolute;
-      bottom: 100%;
-      left: 0;
-      border: 1px solid red;
-    }
+  }
+
+  .content-wrapper {
+    position: absolute;
+    transform: translateY(-100%);
+    border: 1px solid red;
   }
 </style>
